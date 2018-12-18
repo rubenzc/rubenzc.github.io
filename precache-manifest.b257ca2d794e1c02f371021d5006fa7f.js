b789d734063946ca6305b96c8669aa761aaf1124
Rubén <rubenzc@me.com>
2018-12-18 17:34:26 +0100

    First commit

---
+++ precache-manifest.b257ca2d794e1c02f371021d5006fa7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cebb697854823352cdad",
+    "url": "/static/js/main.cebb6978.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "cebb697854823352cdad",
+    "url": "/static/css/main.36625131.chunk.css"
+  },
+  {
+    "revision": "291f07f9391bd9884fa37a9c7284214f",
+    "url": "/index.html"
+  }
+];
